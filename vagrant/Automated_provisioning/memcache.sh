09db116e1c896ec7cdc0955043dd1cd96aae6281
Tomas Abraham <tomiabraham91@gmail.com>
2023-08-10 16:55:28 -0300

    vagrantfile & provisioning scripts

---
+++ vagrant/Automated_provisioning/memcache.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Adding repository and installing MC
+sudo dnf install epel-release -y
+sudo dnf install memcached -y
+
+# starting & enabling MC
+sudo systemctl start memcached
+sudo systemctl enable memcached
+sudo systemctl status memcached
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/sysconfig/memcached
+sudo systemctl restart memcached
+
+#Starting the firewall and allowing MC to access from port 11211
+firewall-cmd --add-port=11211/tcp
+firewall-cmd --runtime-to-permanent
+firewall-cmd --add-port=11111/udp
+firewall-cmd --runtime-to-permanent
+sudo memcached -p 11211 -U 11111 -u memcached -d
